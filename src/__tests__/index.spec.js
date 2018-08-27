260a288b9296f15d065a1453c3680fd10fc3cba4
Krasimir Tsonev <krasimir@outset.ws>
2018-08-27 11:47:19 +0300

    Refactoring

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -1,47 +1,48 @@
-/** @jsx dialectica */
-import dialectica from '../';
+/** @jsx dialect */
+import { dialect, speak } from '../';
 
 describe('Given the Dialectica library', () => {
-  describe('when running a simple dialect function', () => {
-    it('should execute the function with the given params', () => {
-      const Func = jest.fn();
-      const dialect = <Func foo='bar'/>;
+  describe('when running a simple function', () => {
+    it('should execute the function with the given params and return the result', () => {
+      const Func = jest.fn(({ foo }) => foo * 2);
+      const Word = <Func foo={ 10 }/>;
+      const result = speak(Word);
 
-      dialectica.speak(dialect);
+      expect(Func).toBeCalledWith({ foo: 10 });
+      expect(result).toBe(20);
+    });
+    it(`should execute the function with the given merged params
+        return the result even if we use it as a tag`, () => {
+      const Func = jest.fn(({ foo, bar }) => foo * bar);
+      const Word = <Func foo={ 10 }/>;
+      const result = speak(<Word bar={ 20 }/>);
 
-      expect(Func).toBeCalledWith({ foo: 'bar' });
+      expect(Func).toBeCalledWith({ foo: 10, bar: 20 });
+      expect(result).toBe(200);
     });
   });
-  describe('when using the FACC pattern', () => {
-    it('should pass the result of our function to the FACC function', done => {
-      const Func = ({ a, b, c }) => a + b + c;
-      const Dialect = (
-        <Func c={ 5 }>
-          {
-            total => {
-              expect(total).toBe(35);
-              done();
-            }
-          }
-        </Func>
-      );
+  describe('when having nested functions', () => {
+    it('should run all of them and return the result', () => {
+      const Foo = jest.fn();
+      const Bar = jest.fn();
+      const Zar = jest.fn();
+      const language = <Foo><Bar><Zar /></Bar></Foo>;
+      const result = speak(language);
 
-      dialectica.speak(<Dialect a={ 10 } b={ 20 }/>);
+      expect(Foo).toBeCalled();
+      expect(Bar).toBeCalled();
+      expect(Zar).toBeCalled();
     });
-    describe('and we have a nesting of dialects', () => {
-      it('should continue speaking the dialect', () => {
-        const Func = ({ a, b, c }) => a + b + c;
-        const Mult = ({ a }) => a * 10;
-        const Dialect = (
-          <Func c={ 5 }>
-            {
-              total => <Mult a={ total } />
-            }
-          </Func>
-        );
+  });
+  describe('when using FACC pattern', () => {
+    it('should pass down the result of the function as param for the FACC', () => {
+      const Foo = jest.fn(() => 42);
+      const Bar = jest.fn();
+      const language = <Foo>{ answer => <Bar answer={ answer } /> }</Foo>;
+      const result = speak(language);
 
-        expect(dialectica.speak(<Dialect a={ 10 } b={ 20 }/>)).toBe(350);
-      });
-    })
+      expect(Foo).toBeCalled();
+      expect(Bar).toBeCalledWith({ answer: 42 });
+    });
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,46 +1,46 @@
-const DIALECTICA_TYPE = '__dialectica';
+const DIALECT_TYPE = '__dialect';
 
-class Dialect {
+class Word {
   constructor(func, params, children) {
     this.func = func;
     this.params = params;
     this.children = children;
-    this[DIALECTICA_TYPE] = true;
+    this[DIALECT_TYPE] = true;
   }
-  annotate(params) {
-    this.params = Object.assign({}, this.params, params);
-  }
-  static isItDialect(dialect) {
-    return dialect[DIALECTICA_TYPE] === true
-  }
-}
-
-function speak(dialect) {
-  // console.log('-> ', dialect);
-  if (!dialect) return;
-
-  const { func, params, children } = dialect;
-
-  if (typeof func === 'function') {
-    const result = func.call(this, params);
-
-    if (children) {
-      if (children.length === 1 && !Dialect.isItDialect(children[0])) {
-        return speak(children[0](result));
-      } else {
-        children.forEach(speak);
-      }
+  say(params) {
+    if (Word.isItAWord(this.func)) {
+      return this.func.say(this.params);
     }
-    return result;
-  } else if (typeof func === 'object' && Dialect.isItDialect(func)) {
-    func.annotate(params);
-    return speak(func);
+    return this.func.call(this.func, Object.assign({}, this.params, params));
+  }
+  static isItAWord(word) {
+    return word && word[DIALECT_TYPE] === true;
   }
 }
 
-export function Dialect() {}
-export default function dialectica(func, params, ...children) {
-  return new Dialect(func, params, children);
+class Dialect {
+  speak(word) {
+    if (Word.isItAWord(word)) {
+      let result;
+
+      result = word.say();
+
+      if (word.children) {
+        if (word.children.length === 1 && !Word.isItAWord(word.children[0])) {
+          this.speak(word.children[0](result));
+        } else {
+          word.children.forEach(w => this.speak(w));
+        }
+      }
+      return result;
+    }
+  }
+  create(func, params, ...children) {
+    return new Word(func, params, children);
+  }
 }
 
-dialectica.speak = speak;
+const d = new Dialect();
+
+export const dialect = d.create.bind(d);
+export const speak = d.speak.bind(d);
