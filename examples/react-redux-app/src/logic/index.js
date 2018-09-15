12d23ce77acbc83bd72996f77110adea273be1c8
Krasimir Tsonev <krasimir@outset.ws>
2018-09-15 14:07:48 +0300

    Moving forward with the example

--- examples/react-redux-app/src/logic/index.js
+++ examples/react-redux-app/src/logic/index.js
@@ -1,56 +1,17 @@
 /** @jsx D */
-import { D, pipeline } from 'dactory';
-
+import { D, Redux } from 'dactory';
 import { GET_POSTS } from '../redux/constants';
 
-// ****************************************************************
-const { normalizeProps, execute, processChildren } = pipeline;
+const { Subscribe } = Redux;
 
-const ReduxIntegration = {
-  _listeners: [],
-  middleware: function dactoryReduxMiddleware({ getState, dispatch }) {
-    return next => action => {
-      const result = next(action);
-      
-      ReduxIntegration.actionDetected(action);
-      return result;
-    };
-  },
-  addListener(callback) {
-    this._listeners.push(callback);
-  },
-  actionDetected(action) {
-    this._listeners.forEach(l => l(action));
-  }
+function Test(props) {
+  console.log(props);
 }
-
-export function Subscribe(props) {
-  if (props && props.type) {
-    ReduxIntegration.addListener(action => {
-      if (action.type === props.type) {
-        processChildren({ ...this, result: action });
-      }
-    });
-  } else {
-    throw new Error('<Subscribe> requires `type` prop.');
-  }
-}
-Subscribe.pipeline = [
-  normalizeProps,
-  execute
-];
-Subscribe.middleware = ReduxIntegration.middleware;
-
-// ****************************************************************
 
 export default function StartUp() {
   return (
-    <Subscribe type={ 'GET_POSTS' }>
-      {
-        action => {
-          console.log(action);
-        }
-      }
+    <Subscribe type={ GET_POSTS } exports='action'>
+      <Test $action />
     </Subscribe>
   )
 }
--- examples/react-redux-app/src/redux/store.js
+++ examples/react-redux-app/src/redux/store.js
@@ -1,6 +1,6 @@
 import { USERS_FETCHED } from './constants';
 import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducer';
-import { Subscribe } from '../logic';
+import { Redux } from 'dactory';
 
-export default () => createStore(reducer, applyMiddleware(Subscribe.middleware));
+export default () => createStore(reducer, applyMiddleware(Redux.middleware));
